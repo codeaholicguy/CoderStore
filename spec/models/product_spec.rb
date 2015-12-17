081d952c333bf56f42ae26f57f5e177fbffbd156
Nguyễn Nhật Hoàng <hoangnn93@gmail.com>
2015-12-17 20:13:57 +0700

    Milestone 4

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "alphabetical" do
+    Product.create(name: "a")
+    Product.create(name: "d")
+    Product.create(name: "b")
+    expect_products = [Product.find_by(name: "a"), Product.find_by(name: "b"), Product.find_by(name: "d")]
+    expect(Product.alphabetical()).to match_array(expect_products)
+  end
 end
